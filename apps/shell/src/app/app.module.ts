8947436cb977ad3cc93dc5b319dccbf0fbad4479
jeffjin <jeff@jeffjin.com>
2022-09-02 10:41:36 -0600

    npm module updated by removing @nrwl/nx package

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { authRoutes, SecurityModule } from '@eworks/security';
 
@@ -10,7 +9,6 @@ import { authRoutes, SecurityModule } from '@eworks/security';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    NxModule.forRoot(),
     RouterModule.forRoot([{ path: 'auth', children: authRoutes }], { initialNavigation: "enabledNonBlocking" }),
     SecurityModule     // added
   ],
