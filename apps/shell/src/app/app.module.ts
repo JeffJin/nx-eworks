d92736b99aa6df182eb10f4aa719935ae200ffe5
jeffjin <jeff@jeffjin.com>
2022-09-16 01:17:18 -0400

    rename and refactored

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { authRoutes, SecurityModule } from '@eworks/security';
+import { authRoutes, SecurityModule } from '@eworks/ng.security';
 
 @NgModule({
   declarations: [AppComponent],
