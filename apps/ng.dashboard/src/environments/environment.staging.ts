93641179e01d504d8ba3f8ec980873c94c65c774
jeffjin <jeff@jeffjin.com>
2022-10-16 01:11:36 -0400

    nx workspace update, ng dashboard staging environment added

---
+++ apps/ng.dashboard/src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  noBackend: false,
+  apiBaseUrl: 'http://192.168.5.116:5001/api',
+  hubBaseUrl: 'http://192.168.5.116:5001/',
+  facebookAppId: '173860239647801'
+};
--- apps/ng.dashboard/src/environments/environment.ts
+++ apps/ng.dashboard/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   noBackend: false,
-  apiBaseUrl: 'http://192.168.5.116:5001/api',
-  hubBaseUrl: 'http://192.168.5.116:5001/',
+  apiBaseUrl: 'https://localhost:5001/api',
+  hubBaseUrl: 'https://localhost:5001/',
   facebookAppId: '173860239647801'
 };
