2a6bb99da59187934bd60c9193c8691fe38762f6
jeffjin <jeff@jeffjin.com>
2022-10-29 11:23:03 -0400

    staging env updated

--- apps/ng.dashboard/src/environments/environment.staging.ts
+++ apps/ng.dashboard/src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   noBackend: false,
-  apiBaseUrl: 'http://192.168.5.116:5001/api',
-  hubBaseUrl: 'http://192.168.5.116:5001/',
+  apiBaseUrl: 'http://192.168.5.116:5000/api',
+  hubBaseUrl: 'http://192.168.5.116:5000/',
   facebookAppId: '173860239647801'
 };
